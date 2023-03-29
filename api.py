d6978c6f722b23ce4f57794999904ab0f9db310d
Антон Баранов <2603023@gmail.com>
2023-03-29 11:32:47 +0300

    Update api.py

--- api.py
+++ api.py
@@ -6,15 +6,13 @@ from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 
 class PetFriends:
-    """апи библиотека к веб приложению Pet Friends"""
-
+    """API библиотека к веб приложению Pet Friends"""
     def __init__(self):
         self.base_url = "https://petfriends.skillfactory.ru/"
 
     def get_api_key(self, email: str, password: str) -> json:
         """Метод делает запрос к API сервера и возвращает статус запроса и результат в формате
         JSON с уникальным ключем пользователя, найденного по указанным email и паролем"""
-
         headers = {
             'email': email,
             'password': password,
@@ -30,10 +28,9 @@ class PetFriends:
 
     def get_list_of_pets(self, auth_key: json, filter: str = "") -> json:
         """Метод делает запрос к API сервера и возвращает статус запроса и результат в формате JSON
-        со списком наденных питомцев, совпадающих с фильтром. На данный момент фильтр может иметь
+        со списком найденных питомцев, совпадающих с фильтром. На данный момент фильтр может иметь
         либо пустое значение - получить список всех питомцев, либо 'my_pets' - получить список
         собственных питомцев"""
-
         headers = {'auth_key': auth_key['key']}
         filter = {'filter': filter}
 
@@ -48,10 +45,8 @@ class PetFriends:
 
     def add_new_pet(self, auth_key: json, name: str, animal_type: str,
                     age: str, pet_photo: str) -> json:
-        """Метод отправляет (постит) на сервер данные о добавляемом питомце и возвращает статус
+        """Метод отправляет  на сервер данные о добавляемом питомце и возвращает статус
         запроса на сервер и результат в формате JSON с данными добавленного питомца"""
-
-
         data = MultipartEncoder(
             fields={
                 'name': name,
@@ -60,8 +55,6 @@ class PetFriends:
                 'pet_photo': (pet_photo, open(pet_photo, 'rb'), 'image/jpeg')
             })
         headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
-
-
         res = requests.post(self.base_url + 'api/pets', headers=headers, data=data)
         status = res.status_code
         result = ""
@@ -73,12 +66,10 @@ class PetFriends:
         return status, result
 
     def delete_pet(self, auth_key: json, pet_id: str) -> json:
-        """Метод отправляет на сервер запрос на удаление питомца по указанному ID и возвращает
-        статус запроса и результат в формате JSON с текстом уведомления о успешном удалении.
+        """Метод отправляет на сервер запрос на удаление питомца, по указанному ID, и возвращает
+        статус запроса и результат в формате JSON с текстом уведомления об успешном удалении.
         На сегодняшний день тут есть баг - в result приходит пустая строка, но status при этом = 200"""
-
         headers = {'auth_key': auth_key['key']}
-
         res = requests.delete(self.base_url + 'api/pets/' + pet_id, headers=headers)
         status = res.status_code
         result = ""
@@ -91,9 +82,8 @@ class PetFriends:
 
     def update_pet_info(self, auth_key: json, pet_id: str, name: str,
                         animal_type: str, age: int) -> json:
-        """Метод отправляет запрос на сервер о обновлении данных питомуа по указанному ID и
-        возвращает статус запроса и result в формате JSON с обновлённыи данными питомца"""
-
+        """Метод отправляет запрос на сервер об обновлении данных питомца по указанному ID и
+        возвращает статус запроса и result в формате JSON с обновлёнными данными питомца"""
         headers = {'auth_key': auth_key['key']}
         data = {
             'name': name,
@@ -109,8 +99,6 @@ class PetFriends:
         except json.decoder.JSONDecodeError:
             result = res.text
         return status, result
-
-
 
 
     def put_api_pets(self, auth_key, pet_id, name, animal_type, age):
@@ -136,7 +124,6 @@ class PetFriends:
 
     def add_new_pet_without_photo(self, auth_key: json, name: str, animal_type: str, age: str) -> json:
         """Функция добавляет информацию о новом питомце без фото и возвращает информацию в формате JSON"""
-
         headers = {'auth_key': auth_key['key']}
         data = {
             'name': name,
